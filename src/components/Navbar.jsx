da5dc20ea95bd89497b7d064ca413ac18c6ecd87
preeyalr <preeyalrathod44@gmail.com>
2025-07-16 01:36:05 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar ">
+     <div className="w-full flex flex-row  justify-between items-center px-10 py-5"> 
+        <h1>Pallet Rose</h1>
+      <ul className="nav-links text-xs flex flex-row gap-7 justify-between">
+        <li><a href="/">Get Started</a></li>
+        <li><a href="/CreateStrategy">Create Strategy</a></li>
+        <li><a href="/pricing">Pricing</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/Solution">Solution</a></li>
+        <li><a href="/E-Commerce">E-Commerce</a></li>
+        <li><a href="/signup"><img src="..\src\assets\profile.png" width="24" height="24" alt="profile" /></a></li>
+        <li><a href="/setting"><img src="..\src\assets\setting.png" width="24" height="24" alt="setting" /></a></li>
+      </ul></div>
+    </nav>
+  );
+}
