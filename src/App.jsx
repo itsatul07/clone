61d8e83fc753d82079248160bc4c15e3b7cdd66e
Atul Kumar <atul.ak.121202@gmail.com>
2025-07-16 00:59:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {React} from "react";
 
 export default function App(){
     return (
-        <div className=" font-bold text-amber-900">
-        <h1> hello </h1>
+        <div className=" bold-font text-amber-900 text-7xl">
+            <p>hello</p> 
         </div>
     );
 }
