71c2b6a74ea5dcb279fa6fc21b39f7aabbf36cd9
Atul Kumar <atul.ak.121202@gmail.com>
2025-07-16 00:50:58 +0530

    font applied

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {React} from "react";
 
 export default function App(){
     return (
-        <div className="font-[--font-michroma] font-bold text-amber-900">
-        <h1> hello world</h1>
+        <div className=" font-bold text-amber-900">
+        <h1> hello </h1>
         </div>
     );
 }
