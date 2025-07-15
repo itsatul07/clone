d077f541da020ee848f0f9dc348007d6f2911743
Atul Kumar <atul.ak.121202@gmail.com>
2025-07-16 00:46:51 +0530

    still changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {React} from "react";
 
 export default function App(){
     return (
-        <div className="font-[--font-michroma]">
+        <div className="font-[--font-michroma] font-bold text-amber-900">
         <h1> hello world</h1>
         </div>
     );
