173647adbf2fff5be05646515586bdb9fec0f905
Atul Kumar <atul.ak.121202@gmail.com>
2025-07-16 00:45:08 +0530

    font

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {React} from "react";
+
+export default function App(){
+    return (
+        <div className="font-[--font-michroma]">
+        <h1> hello world</h1>
+        </div>
+    );
+}
